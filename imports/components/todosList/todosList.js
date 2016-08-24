2e099cc66c59e21d2a114dd888d822ad930978c8
Julien Tremblay <info@julientremblay.ca>
2016-08-23 22:22:33 -0400

    subscribe + publish p1

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -7,6 +7,7 @@ import template from './todosList.html';
 class TodosListCtrl {
   constructor($scope) {
     $scope.viewModel(this);
+    this.subscribe('tasks');
 
     this.helpers({
       tasks() {
